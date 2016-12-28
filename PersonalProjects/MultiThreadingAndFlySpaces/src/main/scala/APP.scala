56aa6549d6e9b93023d477220631f821cbb3ce40
Eduardo <eduardo.s.rodrigues@ist.utl.pt>
2016-12-28 13:34:12 +0000

    added structure

---
+++ PersonalProjects/MultiThreadingAndFlySpaces/src/main/scala/APP.scala
@@ -0,0 +1,8 @@
+/**
+  * Created by #EduardoRodrigues on 28/12/2016.
+  */
+object APP {
+  def main(args: Array[String]): Unit = {
+    //TODO
+  }
+}
---
+++ PersonalProjects/MultiThreadingAndFlySpaces/src/main/scala/Cashier.scala
@@ -0,0 +1,6 @@
+/**
+  * Created by #ScalaTeam on 28/12/2016.
+  */
+class Cashier {
+
+}
